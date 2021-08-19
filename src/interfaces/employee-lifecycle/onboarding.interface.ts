bc9ade67d9e6ac97d46fca7ee2ff1eb173c17c4e
tooney92 <anthony.ime@outsourceglobal.com>
2021-08-19 09:19:23 +0100

    added interfaces for employee lifecycle

---
+++ src/interfaces/employee-lifecycle/onboarding.interface.ts
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+export interface OnBoarding {
+    jobApplicant: string;
+    employee: string;
+    status: string;
+    employeeOnboardingTemplate: string;
+    department: string;
+    designation: string;
+    employee_grade: string;
+    campaign: string;
+    branch: string;  
+    dateOfJoining: Date;
+  }
+  
