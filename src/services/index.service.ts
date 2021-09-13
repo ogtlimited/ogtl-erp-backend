a8c6671ad62152fab5d9cc317f77a338f577dc73
Sir Abubakar <abubakar.abdulwahab@outsourceglobal.com>
2021-09-13 15:03:03 +0100

    department bug

--- src/services/index.service.ts
+++ src/services/index.service.ts
@@ -1,7 +1,8 @@
 /* eslint-disable prettier/prettier */
 import  shiftTypeService  from '@/services/shift/shift.service';
-import DepartmentService  from '@/services/employee/department.service';
+
 import  DesignationService  from '@/services/employee/designation.service';
+import  DepartmentService  from '@/services/employee/department.service';
 import EmployeeService from './employee.service';
 import ProjectService from '@services/project/project.service';
 import ClientService from '@services/project/client.service';
@@ -9,25 +10,25 @@ import JobApplicantService from '@services/recruitment/job_applicant.service';
 
 class CombineServices {
   public designationS = new DesignationService();
-  public departmentS = new DepartmentService();
+  // public departmentS = new DepartmentService();
   public shiftS = new shiftTypeService();
   public employeeS = new EmployeeService()
   public projectS = new ProjectService()
   public clientS  = new ClientService()
   public jobApplicantS = new JobApplicantService()
-//   public departmentS = new Department
+  // public departmentS = new DepartmentService()
 
   public async createEmployeeFormSelection(){
     const shifts = await this.shiftS.findAllshiftType()
     const employees = await this.employeeS.findAllEmployee()
     const designations = await this.designationS.findAllDesignations()
-    const departments = await this.departmentS.findAllDepartments()
+    // const departments = await this.departmentS.findAllDepartments()
     const projects = await this.projectS.findAll();
     const jobApplicants = await this.jobApplicantS.findAllJobApplicants()
     return {
         shifts: shifts,
         designations: designations,
-        departments,
+        // departments,
         employees,
         projects,
         jobApplicants
