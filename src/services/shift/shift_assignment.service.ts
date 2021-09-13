f8d97df78c01dabb23ec6ce45d26d3c06eb85d17
Ahmed Dambatta Hassan <62890621+ameerdbt@users.noreply.github.com>
2021-09-13 19:42:06 +0100

    fixed shift assignment bug

--- src/services/shift/shift_assignment.service.ts
+++ src/services/shift/shift_assignment.service.ts
@@ -33,7 +33,7 @@ class ShiftAssignmentService {
     if (isEmpty(shiftAssignmentData)) throw new HttpException(400, "Bad request");
     //find shift assignment using the employee id provided
     const findShitAssignment: IShiftAssignment = await this.shiftAssignment.findOne({ employee_id: shiftAssignmentData.employee_id });
-    this.employeeModel.findOneAndUpdate({_id: shiftAssignmentData.employee_id}, {$set: {default_shift:shiftAssignmentData.shift_type_id}}, { new: true })
+    await this.employeeModel.findOneAndUpdate({_id: shiftAssignmentData.employee_id}, {$set: {default_shift:shiftAssignmentData.shift_type_id}}, { new: true })
 
     //throw error if shift assignment does exist
     if (findShitAssignment) throw new HttpException(409, `Shift assignment already exists`);
