67df786f7e94011749c533c5ecdad277fdd61b7e
anthugny <anthony.ime@outsourceglobal.com>
2021-09-16 01:14:00 +0100

    Revert "Revert "added payroll entry""
    This reverts commit cf2f5a5646c854deca3074888d5ea20cfffab59b.

--- src/utils/pipelineUtils.ts
+++ src/utils/pipelineUtils.ts
@@ -1,49 +1,48 @@
 import { ObjectId } from 'mongodb';
 export const deductionAggBuilder = employeeId => {
-    const query = [
-      {
-        $facet: {
-          deductionIds: [
-            {
-              $project: {
-                amount: 0,
+  const query = [
+    {
+      $facet: {
+        deductionIds: [
+          {
+            $project: {
+              amount: 0,
+            },
+          },
+          {
+            $match: {
+              employeeId: new ObjectId(employeeId),
+            },
+          },
+          {
+            $project: {
+              _id: 1,
+            },
+          },
+        ],
+        totalDeductions: [
+          {
+            $project: {
+              amount: 1,
+              employeeId: 1,
+            },
+          },
+          {
+            $match: {
+              employeeId: new ObjectId(employeeId),
+            },
+          },
+          {
+            $group: {
+              _id: 'totalamount',
+              sum: {
+                $sum: '$amount',
               },
             },
-            {
-              $match: {
-                employeeId: new ObjectId(employeeId),
-              },
-            },
-            {
-              $project: {
-                _id: 1,
-              },
-            },
-          ],
-          totalDeductions: [
-            {
-              $project: {
-                amount: 1,
-                employeeId: 1,
-              },
-            },
-            {
-              $match: {
-                employeeId: new ObjectId(employeeId),
-              },
-            },
-            {
-              $group: {
-                _id: 'totalamount',
-                sum: {
-                  $sum: '$amount',
-                },
-              },
-            },
-          ],
-        },
+          },
+        ],
       },
-    ];
-    return query;
-  };
-  
+    },
+  ];
+  return query;
+};
