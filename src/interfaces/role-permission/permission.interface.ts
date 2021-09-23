552a990aec1c2b6d19273b1106479658058a3eb1
Sir Abubakar <abubakar.abdulwahab@outsourceglobal.com>
2021-09-23 10:06:43 +0100

    update

--- src/interfaces/role-permission/permission.interface.ts
+++ src/interfaces/role-permission/permission.interface.ts
@@ -1,8 +1,9 @@
+/* eslint-disable prettier/prettier */
 export interface IRoleAndPermission {
-  _id:string;
-  role:string;
-  create:boolean;
-  read:boolean;
-  update:boolean;
-  delete:boolean;
+  _id: string;
+  role: string;
+  create: boolean;
+  read: boolean;
+  update: boolean;
+  delete: boolean;
 }
