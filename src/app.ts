ae830d007a5f47b7b6ceaa89e7c63e4242ed83db
Sir Abubakar <abubakar.abdulwahab@outsourceglobal.com>
2021-11-04 01:37:43 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -110,7 +110,7 @@ class App {
 
   private initializeMiddlewares() {
     this.app.use(morgan(config.get('log.format'), { stream }));
-    const allowedOrigins = ['https://erp-api.outsourceglobal.com/api/login', 'https://erp.outsourceglobal.com'];
+    const allowedOrigins = ['https://erp.outsourceglobal.com'];
     const options: cors.CorsOptions = {
       origin: allowedOrigins
     };
