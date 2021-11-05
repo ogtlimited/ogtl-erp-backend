3c5484d4a05f54c6196e1695b88640ea81cb7a33
Nnamdi ogundu <59602267+unicorncodak@users.noreply.github.com>
2021-11-05 10:07:49 +0100

    commit

--- src/app.ts
+++ src/app.ts
@@ -76,7 +76,7 @@ class App {
           socket.on('disconnect', (data) => {
               console.log('Client disconnected')
           })
-
+        
       })
   }
 
@@ -101,7 +101,6 @@ class App {
   }
 
   private connectToDatabase() {
-    console.log(process.env.databaseUrl);
     if (this.env !== 'production') {
       set('debug', true);
     }
@@ -209,7 +208,7 @@ class App {
       const attendanceService = new AttendanceTypeService()
       await attendanceService.generateAttendance()
     //   console.log('running task 1am every day');
-    //   const day = "saturday"
+    //   const day = "saturday" 
     //   if (day == "saturday" || day == "sunday") {
     //     console.log("skipping today")
     //   }else{
