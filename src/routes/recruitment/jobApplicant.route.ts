113d89b2c2c142bf5b9ee6162d12a5453ab90c1e
Sir Abubakar <abubakar.abdulwahab@outsourceglobal.com>
2021-11-29 12:46:52 +0100

    Update jobApplicant.route.ts

--- src/routes/recruitment/jobApplicant.route.ts
+++ src/routes/recruitment/jobApplicant.route.ts
@@ -22,12 +22,12 @@ class JobApplicantRoute implements Routes {
     this.router.get(`${this.path}-accepted`,authMiddleware, this.jobApplicantController.getAcceptedJobApplicants);
     this.router.post(
       `${this.path}`,
-      [validationMiddleware(CreateJobApplicantDto, 'body'), authMiddleware],
+      [validationMiddleware(CreateJobApplicantDto, 'body')],
       this.jobApplicantController.createJobApplicant,
     );
     this.router.patch(
       `${this.path}/:id`,
-      [validationMiddleware(UpdateJobApplicantDto, 'body'), authMiddleware],
+      [validationMiddleware(UpdateJobApplicantDto, 'body')],
       this.jobApplicantController.updateJobApplicant,
     );
     this.router.delete(`${this.path}/:id`, authMiddleware, this.jobApplicantController.deleteJobApplicant);
