9966319db51270004010f452bbe2bac6206965f5
anthugny <anthony.ime@outsourceglobal.com>
2022-02-10 09:50:06 +0100

    setup job applications

---
+++ src/interfaces/recruitment/job-applications-task.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+export interface IJobApplicationsTasks {
+  _id: string;
+  in_house_agent: string;
+  sieving: Number;
+  phone_screening: Number;
+  scheduled_for_interview: Number;
+}
