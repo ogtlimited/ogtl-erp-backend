e2eaf6e6f9e6076144cb7b28507530f5677434e8
anthugny <anthony.ime@outsourceglobal.com>
2022-03-08 09:29:23 +0100

    added update for employee salary

--- src/services/payroll/salary-slip.service.ts
+++ src/services/payroll/salary-slip.service.ts
@@ -162,7 +162,7 @@ class SalarySlipService {
     const records = [];
     const today = new Date()
     if(today.getDate() != 25){
-      throw new HttpException(401, "Cannot generate slip before the 25th!")
+      throw new HttpException(400, "Cannot generate slip before the 25th!")
     }
     const noSalaries = []
     const employeeSalaries = await employeesSalaryModel.find({}).populate(
