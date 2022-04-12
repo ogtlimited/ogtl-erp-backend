9ca40d1876cf856c67c231d775f59b3f7a066db8
odogwuval2 <valentine.madu@outsourceglobal.com>
2022-04-12 05:54:37 +0100

    modify pipeline script

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -4,5 +4,5 @@ cd /home/ubuntu/ogtl-erp-backend
 cp /home/ubuntu/.env .
 sudo chown -R ubuntu:ubuntu ./*
 sudo apt install npm -y
-npm install pm2 -g 
+sudo npm install pm2 -g 
 pm2 startOrReload ecosystem.config.js --only prod
