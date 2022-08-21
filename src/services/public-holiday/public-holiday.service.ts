89c0ccdb1365bf53a6aceb08d941cf6656c44ed8
Clinton Otse <otseclinton60@gmail.com>
2022-08-22 00:58:05 +0100

    fix

--- src/services/public-holiday/public-holiday.service.ts
+++ src/services/public-holiday/public-holiday.service.ts
@@ -2,8 +2,8 @@ import { CreatePublicHolidayDto } from '@/dtos/public-holiday/public_holiday.dto
 import { IPublicHoliday } from '@/interfaces/public-holiday/public_holiday.interface';
 import publicHolidayModel from '@/models/public-holiday/public_holiday.model';
 import { HttpException } from '@/exceptions/HttpException';
-import { isEmpty } from '@/utils/util';
 import omit from 'lodash/omit';
+// import { isEmpty } from '@/utils/util';
 import moment from 'moment';
 
 class PublicHolidayService {
@@ -17,20 +17,11 @@ class PublicHolidayService {
       throw new HttpException(409, `${newData.title} Public holiday already exists`);
     }
 
-    const newPublicHolidayData: CreatePublicHolidayDto = {
-      startDate: newData.startDate,
-      endDate: newData.endDate,
-      title: newData.title,
-      project_id: user.projectId,
-      updated_by: null,
-      created_by: user._id,
-      deleted_by: null,
-      deleted: false,
-    };
-
-    let publicHoliday = await publicHolidayModel.create(newPublicHolidayData);
+    let publicHoliday = await publicHolidayModel.create(newData);
     console.log(`${newData.title} Public holiday has been created`, publicHoliday);
     publicHoliday = omit(publicHoliday.toObject(), ['updated_by', 'deleted_by']);
+
+    console.log('This user created this public holiday', user._id);
 
     return publicHoliday;
   }
