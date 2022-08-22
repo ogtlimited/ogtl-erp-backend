14717b853bf76ba39d67f7e2905e94c7447be8a9
Clinton Otse <otseclinton60@gmail.com>
2022-08-22 01:24:52 +0100

    fix

--- src/dtos/public-holiday/public_holiday.dto.ts
+++ src/dtos/public-holiday/public_holiday.dto.ts
@@ -1,4 +1,4 @@
-import { IsDateString, IsOptional, IsString } from 'class-validator';
+import { IsBoolean, IsDateString, IsOptional, IsString } from 'class-validator';
 
 export class CreatePublicHolidayDto {
   @IsDateString()
@@ -13,4 +13,19 @@ export class CreatePublicHolidayDto {
   @IsString()
   @IsOptional()
   public project_id: string;
+
+  @IsString()
+  @IsOptional()
+  public updated_by: string;
+
+  @IsString()
+  public created_by: string;
+
+  @IsString()
+  @IsOptional()
+  public deleted_by: string;
+
+  @IsBoolean()
+  @IsOptional()
+  public deleted: boolean;
 }
--- src/interfaces/public-holiday/public_holiday.interface.ts
+++ src/interfaces/public-holiday/public_holiday.interface.ts
@@ -4,7 +4,7 @@ export interface IPublicHoliday {
   project_id?: string;
   title: string;
   updated_by?: string;
-  created_by?: string;
+  created_by: string;
   deleted_by?: string;
   deleted: boolean;
 }
