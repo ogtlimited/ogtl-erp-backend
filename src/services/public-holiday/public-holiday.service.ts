dfc42ab44e8bf8155737fc6ccf62b4639457c741
Clinton Otse <otseclinton60@gmail.com>
2022-08-22 01:43:04 +0100

    Route - Create

--- src/services/public-holiday/public-holiday.service.ts
+++ src/services/public-holiday/public-holiday.service.ts
@@ -4,12 +4,11 @@ import publicHolidayModel from '@/models/public-holiday/public_holiday.model';
 import { HttpException } from '@/exceptions/HttpException';
 import omit from 'lodash/omit';
 // import { isEmpty } from '@/utils/util';
-import moment from 'moment';
 
 class PublicHolidayService {
   public publicHolidayModel = publicHolidayModel;
 
-  public async create(user, publicHolidayData: CreatePublicHolidayDto): Promise<IPublicHoliday> {
+  public async create(req, publicHolidayData: CreatePublicHolidayDto): Promise<IPublicHoliday> {
     const newData = publicHolidayData;
 
     const publicHolidayExists = await publicHolidayModel.exists(newData);
@@ -17,11 +16,12 @@ class PublicHolidayService {
       throw new HttpException(409, `${newData.title} Public holiday already exists`);
     }
 
+    newData.created_by = req.user._id;
+    newData.project_id = req.user.projectId._id;
+
     let publicHoliday = await publicHolidayModel.create(newData);
     console.log(`${newData.title} Public holiday has been created`, publicHoliday);
     publicHoliday = omit(publicHoliday.toObject(), ['updated_by', 'deleted_by']);
-
-    console.log('This user created this public holiday', user._id);
 
     return publicHoliday;
   }
