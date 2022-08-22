bd1ed3ed8ecc98ae74760f870261e760266f16b0
Clinton Otse <otseclinton60@gmail.com>
2022-08-22 11:11:59 +0100

    Route - update

--- src/routes/public-holiday/public-holiday.route.ts
+++ src/routes/public-holiday/public-holiday.route.ts
@@ -4,7 +4,7 @@ import { CreatePublicHolidayDto } from '@dtos/public-holiday/public_holiday.dto'
 import PublicHolidayController from '@controllers/public-holiday/public-holiday.controller';
 import authMiddleware from '@/middlewares/auth.middleware';
 import validationMiddleware from '@middlewares/validation.middleware';
-// import permissionMiddleware from '@/middlewares/permission.middleware';
+import permissionMiddleware from '@/middlewares/permission.middleware';
 
 class PublicHolidayRoute implements Routes {
   public path = '/api/public_holidays';
@@ -19,7 +19,12 @@ class PublicHolidayRoute implements Routes {
     this.router.post(`${this.path}`, [authMiddleware, validationMiddleware(CreatePublicHolidayDto, 'body')], this.publicHolidayController.create);
     this.router.get(`${this.path}/list`, [authMiddleware], this.publicHolidayController.findAll);
     this.router.get(`${this.path}/list/:active`, [authMiddleware], this.publicHolidayController.findAllActive);
-    this.router.get(`${this.path}/list/:id`, [authMiddleware], this.publicHolidayController.findById);
+    this.router.get(`${this.path}/:id`, [authMiddleware], this.publicHolidayController.findById);
+    this.router.patch(
+      `${this.path}/:id`,
+      [authMiddleware, validationMiddleware(CreatePublicHolidayDto, 'body'), permissionMiddleware('HR')],
+      this.publicHolidayController.update,
+    );
   }
 }
 
