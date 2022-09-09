4885cb59732c53d47d749afaf898b83903ae0f21
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-09-09 00:20:22 -0700

    include required option in the public_holiday_id field

--- src/models/office-holiday/office_holiday.model.ts
+++ src/models/office-holiday/office_holiday.model.ts
@@ -1,20 +1,19 @@
 import { model, Schema } from 'mongoose';
 
 const officeHolidaySchema: Schema = new Schema({
-  project_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'Project',
-  },
-
-  department_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'Department',
-  },
-
   public_holiday_id: {
     type: Schema.Types.ObjectId,
     ref: 'Public_holiday',
+    required: true,
   },
+  project_id: { 
+    type: Schema.Types.ObjectId,
+    ref: 'Project',
+  },
+  department_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'Department',
+  }
 });
 
 const officeHolidayModel = model('office_holiday', officeHolidaySchema);
