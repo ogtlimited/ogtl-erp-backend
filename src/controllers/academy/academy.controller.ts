a1d77663ac750a2f4b977aa0a3839448a513e134
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-09-16 15:17:13 -0700

    Creating academy controller

---
+++ src/controllers/academy/academy.controller.ts
@@ -0,0 +1,42 @@
+/* eslint-disable prettier/prettier */
+import { NextFunction, Request, Response } from 'express';
+import AcademyService from "@services/academy/academy.service";
+
+
+class AcademyController {
+
+  public newAcademyService = new AcademyService();
+  public findAll = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const findAllRecords = await this.newAcademyService.findAll(req.query);
+      res.status(200).json({ data: findAllRecords});
+    } catch (error) {
+      next(error);
+    }
+  };
+
+//   public findById = async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const username: string = req.params.username;
+//       const data = await this.newAcademyService.findById(username);
+//       res.status(200).json({ data: data});
+//     } catch (error) {
+//       next(error);
+//     }
+//   };
+
+  public create = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const newData = req.body;
+      const createdData = await this.newAcademyService.create(newData);
+      res.status(201).json({ data: createdData});
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  };
+
+ 
+}
+
+export default AcademyController;
