4265c84a275c878612e995237c6189c1d2581050
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-09-16 16:25:54 -0700

    Cleaning up service

--- src/services/academy/academy.service.ts
+++ src/services/academy/academy.service.ts
@@ -9,32 +9,19 @@ class AcademyService {
     return academyRecords;
   }
 
-//   public async findById(username: string): Promise<any>{
-//     const academyRecord= await this.academyModel.findOne({ username: username });
-//     if (!academyRecord) throw new HttpException(404, 'no record found');
-//     return academyRecord;
-//   }
-
   public async create(info): Promise<any> {
-    // const {data} = info
     const academyRecords = [];
     const nonExistingRecords = []
     for(let idx = 0; idx < info.length; idx++){
       const record = info[idx]
 
-    //   const academyInfo  = await academyModel.find({"record.username":{$exists:true}})
-      const academyInfo  = await academyModel.findOne({username: record.username})
+      const academyInfo  = await academyModel.findOne({user_name: record.user_name})
 
-    //   console.log("academy info status",academyInfo)
       if(academyInfo === null){
         academyRecords.push(record)
       }
-    //   if(!academyInfo){
-    //     academyRecords.push(record)
-    //     continue
-    //   }
+   
     }
-    // console.log(nonExistingRecords);
     await academyModel.insertMany(academyRecords)
     return `${academyRecords.length} record(s) uploaded successfully`
   }
