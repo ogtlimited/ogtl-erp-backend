1e3bcb1cb428568105b25f5a6b9419f141c0990e
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-09-24 01:23:28 -0700

    Filtering fields from google form

--- src/controllers/academy/academy.controller.ts
+++ src/controllers/academy/academy.controller.ts
@@ -1,10 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { NextFunction, Request, Response } from 'express';
 import AcademyService from "@services/academy/academy.service";
+import { send } from 'process';
 
 
 class AcademyController {
-
+    
   public newAcademyService = new AcademyService();
   public findAll = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -36,20 +37,52 @@ class AcademyController {
     }
   };
 
-
   public createFromGoogleForm = async (req: Request, res: Response, next: NextFunction) => {
     try {
+
+       if (req.body['Please upload your CV.']){
+            req.body['Please upload your CV.'] =  req.body['Please upload your CV.']
+           .slice(req.body['Please upload your CV.'].indexOf("https")
+           ,req.body['Please upload your CV.'].indexOf("'>File"))
+       }
+       if (req.body['Certifications in relation to the selected program above (if any)']){
+       req.body['Certifications in relation to the selected program above (if any)'] = req.body['Certifications in relation to the selected program above (if any)'].split("<BR/>")
+       }
+       if (req.body['Favored Programming Language(s)']){
+       req.body['Favored Programming Language(s)'] = req.body['Favored Programming Language(s)'].split("<BR/>")
+       }
+      
       const newData = req.body;
-    //   const createdData = await this.newAcademyService.create(newData);
-     console.log("From Google Form", newData)
-    //   res.status(201).json({ data: createdData});
-      res.sendStatus(200);
+      const formattedRequest= {
+        application_date: newData['Created At'],
+        gender: newData['Please select your gender'],
+        user_name: newData['email'],
+        first_name: newData['First Name'],
+        middle_name: newData['Middle Name'],
+        last_name: newData['Last Name'],
+        mobile_number: newData['Mobile Number'],
+        alt_mobile_number: newData['Alternate Phone Number'],
+        highest_qualification_attained: newData['Highest Qualification Attained'],
+        other_option: newData["If 'Other' was selected above, please state which."],
+        interested_program: newData['Interested program'],
+        mode_of_engagement: newData['What mode of engagement would you prefer '],
+        weekly_hours: newData['How many hours in a week can you commit to this program '],
+        stack: newData['What is your Stack?'],
+        fav_programming_lang: newData['Favored Programming Language(s)'],
+        years_of_experience: newData['Years of experience in the selected program above '],
+        certifications: newData['Certifications in relation to the selected program above (if any)'],
+        cv: newData['Please upload your CV.'],
+        consent: newData['Consent'],
+        email: newData['Email'],
+  }
+
+      const createdData = await this.newAcademyService.create(newData);
+      res.status(201).json({ data: createdData});
     } catch (error) {
       console.log(error);
       next(error);
     }
   };
-
  
 }
 
