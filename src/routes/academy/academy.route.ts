888fac717013b63ea82e80c32ef408ab90379670
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-09-26 10:08:32 -0700

    creating a findAllById, update, delete  route

--- src/routes/academy/academy.route.ts
+++ src/routes/academy/academy.route.ts
@@ -7,16 +7,19 @@ import authMiddleware from '../../middlewares/auth.middleware';
 class AcademyRoute implements Routes {
     public path = '/api/academy';
     public router = Router();
-    public newAcademyController = new AcademyController();
+    public AcademyController = new AcademyController();
 
     constructor() {
       this.initializeRoutes();
     }
 
     private initializeRoutes() {
-        this.router.get(`${this.path}`, [authMiddleware], this.newAcademyController.findAll);
-        this.router.post(`${this.path}`,[authMiddleware], this.newAcademyController.create);
-        this.router.post(`${this.path}/google_form`, this.newAcademyController.createFromGoogleForm);
+        this.router.get(`${this.path}`, [authMiddleware], this.AcademyController.findAllAcademyApplicants);
+        this.router.get(`${this.path}/:id`, [authMiddleware], this.AcademyController.findAcademyApplicantById);
+        this.router.patch(`${this.path}/update/:id`, [authMiddleware], this.AcademyController.updateAcademyApplicant);
+        this.router.post(`${this.path}`,[authMiddleware], this.AcademyController.create);
+        this.router.post(`${this.path}/google_form`,[authMiddleware], this.AcademyController.createFromGoogleForm);
+        this.router.delete(`${this.path}/delete/:id`, authMiddleware,this.AcademyController.deleteAcademyApplicant);
     }
   }
   export default AcademyRoute;
