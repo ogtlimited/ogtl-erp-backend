dbc1a2b48c02eab046f0c44acb8107f9fea0e6fb
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-11-16 15:37:25 +0100

    update client_account service

--- src/services/project/client_account.service.ts
+++ src/services/project/client_account.service.ts
@@ -74,10 +74,10 @@ class ClientAccountService {
     private async mailSender(id: string, clientEmail: string): Promise<any> {
         const { host }: dbConfig = config.get('dbConfig');
         const port = 3001
-        // const logo = "./utils/logo/logo.png"
-        // const imageTag = `<img src=${logo} alt="Outsource Global Technology" width="500" height="600">`
+        const logo = "/utils/logo/logo.png"
+        const imageTag = `<img src=${logo} alt="Outsource Global Technology" width="200" height="100">`
         const url = `http://${host}:${port}/auth/activate?id=${id}`
-        const html = `<div><h1>Outsource Global Technology</h1></div><p>${clientAccountActivationNotice.message}</p><a href=${url}>Click here</a>`
+        const html = `<div><h1 style="color:#00c2fa">Outsource Global Technology</h1><br></div><p>${clientAccountActivationNotice.message}</p><a href=${url}>Click here</a>`
         return ClientEmail.sendMailToClient(clientEmail,"abubakar.moses@outsourceglobal.com",clientAccountActivationNotice.subject,clientAccountActivationNotice.message,html)
     }
     private async createJwtToken(id: string, clientEmail: string): Promise<any> {
