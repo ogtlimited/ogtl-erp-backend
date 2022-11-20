d2d2e4dde9b612795929729d7df77db0990f0366
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-11-20 12:41:20 +0100

    update client account dto

--- src/dtos/project/client_account.dto.ts
+++ src/dtos/project/client_account.dto.ts
@@ -1,32 +1,48 @@
-import {IsString, IsNotEmpty, IsOptional, IsBoolean, IsEmail, Length} from "class-validator"
+import {IsString, IsNotEmpty, IsOptional, IsBoolean, IsEmail, Length, IsEmpty} from "class-validator"
+
 export class ClientAccountDto{
-    @IsString()
-    @IsNotEmpty()
-    user_name: string
+        @IsString()
+        @IsOptional()
+        _id: string
+        
+        @IsString()
+        @IsNotEmpty()
+        user_name: string
 
-    @IsString()
-    @IsEmail()
-    @IsNotEmpty()
-    email: string;
+        @IsString()
+        @IsEmail()
+        @IsNotEmpty()
+        email: string;
 
-    @IsString()
-    @IsOptional()
-    password: string;
+        @IsString()
+        @IsOptional()
+        password: string;
 
-    @IsBoolean()
-    @IsOptional()
-    activated: boolean;
-    
-    @IsBoolean()
-    @IsOptional()
-    spammy: boolean;
+        @IsBoolean()
+        @IsOptional()
+        activated: boolean;
+        
+        @IsBoolean()
+        @IsOptional()
+        spammy: boolean;
 
-    @IsString()
-    @IsNotEmpty()
-    client_id: string
-    }
+        @IsString()
+        @IsNotEmpty()
+        client_id: string
+        }
 
     export class UpdateClientAccountDto{
+        @IsString()
+        @IsOptional()
+        user_name: string
+        
+        @IsString()
+        @IsEmail()
+        @IsOptional()
+        email: string;
+        }
+
+    export class ResetClientAccountPasswordDto{
     
         @IsString()
         @IsNotEmpty()
