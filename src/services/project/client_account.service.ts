1fae3e6b08fddfe3055c9a1871c85f8076491ce6
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-11-20 12:27:04 +0100

    update client account service

--- src/services/project/client_account.service.ts
+++ src/services/project/client_account.service.ts
@@ -2,24 +2,21 @@
 import { IClientAccount } from '@/interfaces/project-interface/client_account.interface';
 import { HttpException } from '@exceptions/HttpException';
 import { isEmpty } from '@utils/util';
-import { ClientAccountDto, UpdateClientAccountDto } from '@/dtos/project/client_account.dto';
+import { ClientAccountDto } from '@/dtos/project/client_account.dto';
 import clientAccountModel from '@/models/project/client_account.model';
 import clientModel from '@/models/project/client.model';
 const bcrypt = require('bcrypt')
 import { clientAccountActivationNotice } from '@/utils/message';
-import ClientEmail from '@/utils/client.email';
+import SendMail from '@/utils/email.service';
 import { dbConfig } from '@interfaces/db.interface';
 import config from 'config';
 const jwt  = require('jsonwebtoken');
+import _ from "lodash"
 
 
 class ClientAccountService {
-    public clientAccount: any;
-    public client: any;
-    constructor() {
-        this.clientAccount = clientAccountModel;
-        this.client = clientModel;
-    }
+    private clientAccount: any = clientAccountModel;
+    private client: any = clientModel
 
     public async findAllClientsAccounts(): Promise<IClientAccount[]> {
         const clientsAccount: IClientAccount[] = await this.clientAccount.find();
@@ -35,25 +32,35 @@ class ClientAccountService {
     }
 
     public async createClientAccount(payload: ClientAccountDto): Promise<IClientAccount> {
-        interface ClientId {
-            _id: string
-        }
         if (isEmpty(payload)) throw new HttpException(400, "Bad request");
         const clientAccountExist = await this.clientAccount.findOne({email: payload.email})
         if(clientAccountExist)  throw new HttpException(422, "Client Login Details Already Exist");
-        const newclientAccount: IClientAccount & ClientId = await this.clientAccount.create(payload);
-        if(newclientAccount){
-            await this.createJwtToken(newclientAccount._id, newclientAccount.email)
-            return newclientAccount;
+        const newclientAccount: any = await this.clientAccount.create(payload);
+        const recordsWithoutPassword = _.omit(newclientAccount.toObject(),['password'])
+        if(newclientAccount) {
+            const verifyToken = await this.createJwtToken(newclientAccount._id)
+            await this.sendMail(verifyToken.id, newclientAccount.email, newclientAccount.user_name)
+            return recordsWithoutPassword ;
         }
+    }
+    public async resetClientAccountPassword(clientAccountId: string, payload: ClientAccountDto): Promise<IClientAccount> {
+        const salt = await bcrypt.genSalt(10)
+        if (isEmpty(payload)) throw new HttpException(400, "Bad request");
+        payload.password = await bcrypt.hash(payload.password, salt)
+        const findclientAccountAndResetPassword = await this.clientAccount.updateOne(
+            {
+            _id: clientAccountId
+            },
+            {
+                $set: {password: payload.password}
+            });
+            return findclientAccountAndResetPassword;
     }
 
     public async updateClientAccount(clientAccountId: string, payload: ClientAccountDto): Promise<IClientAccount> {
-        const salt = await bcrypt.genSalt(10)
         if (isEmpty(payload)) throw new HttpException(400, "Bad request");
         const findclientAccount = await this.clientAccount.findOne({_id: clientAccountId});
         if (!findclientAccount) throw new HttpException(404, "Client login details not found");
-        payload.password = await bcrypt.hash(payload.password, salt)
         const updateclientAccountDetails: IClientAccount = await this.clientAccount.findByIdAndUpdate(clientAccountId, payload, 
             {
                 new: true,
@@ -62,33 +69,31 @@ class ClientAccountService {
         return updateclientAccountDetails;
     }
 
-    public async activatingClientAccount(clientAccountId: string): Promise<IClientAccount> {  
-        const findclientAccountRecords = await this.clientAccount.findOne({_id: clientAccountId});
-        findclientAccountRecords.activated = findclientAccountRecords.activated ? false : true
-        await findclientAccountRecords.save()
+    public async activateClientAccount(clientAccountId: string): Promise<IClientAccount> {  
+        const findclientAccountRecords: IClientAccount = await this.clientAccount.updateOne({_id: clientAccountId}, {$set:{activated: true}});
+        if(!findclientAccountRecords) throw new HttpException(404, "Cient account details not found");
         return findclientAccountRecords;
     }
 
-    public async deleteClientAccount(clientAccountId: string): Promise<IClientAccount> {
-        const drop: IClientAccount = await this.clientAccount.findByIdAndDelete(clientAccountId);
-        if (!drop) throw new HttpException(409, `${clientAccountId} does not exist`);
-        return drop;
+    public async deactivateClientAccount(clientAccountId: string): Promise<IClientAccount> {
+        const findclientAccountRecords: IClientAccount = await this.clientAccount.updateOne({_id: clientAccountId}, {$set:{activated: false}});
+        if(!findclientAccountRecords) throw new HttpException(404, "Cient account details not found");
+        return findclientAccountRecords;
     }
    
-    private async mailSender(id: string, clientEmail: string): Promise<any> {
+    private async sendMail(id: string, clientEmail: string, username: string): Promise<any> {
+        const clientAccountActivationNoticeObj = clientAccountActivationNotice(username)
         const { host }: dbConfig = config.get('dbConfig');
         const port = 3001
-        const logo = "/utils/logo/logo.png"
-        const imageTag = `<img src=${logo} alt="Outsource Global Technology" width="200" height="100">`
         const url = `http://${host}:${port}/auth/activate?id=${id}`
-        const html = `<div><h1 style="color:#00c2fa">Outsource Global Technology Limited</h1><br></div><p>${clientAccountActivationNotice.message}</p><a href=${url}>Click here</a>`
-        return ClientEmail.sendMailToClient(clientEmail,"hr@outsourceglobal.com",clientAccountActivationNotice.subject,clientAccountActivationNotice.message,html)
+        const html = `<div><h1 style="color:#00c2fa">Outsource Global Technology Limited</h1><br></div>${clientAccountActivationNoticeObj.message}<a href=${url}>Click here to change your password</a>`
+        return SendMail.sendMail(clientEmail,"hr@outsourceglobal.com",clientAccountActivationNoticeObj.subject,clientAccountActivationNoticeObj.message,html)
     }
-    private async createJwtToken(id: string, clientEmail: string): Promise<any> {
+    
+    private async createJwtToken(id: string, expiresIn: number = 24*60*60): Promise<any> {
         const secretKey: string = await config.get('secretKey');
-        const token = await jwt.sign({ id: id }, secretKey,{ expiresIn: "3 days" });
-        const verifyToken = await jwt.verify(token, secretKey);
-        this.mailSender(verifyToken.id, clientEmail)
+        const token = await jwt.sign({ id }, secretKey,{ expiresIn: expiresIn });
+        return await jwt.verify(token, secretKey);
     }
 }
 
