6b8af48c8b551734a5991c496e0e62dd43808d6a
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-11-21 13:05:44 +0100

    Update client account route

--- src/routes/project/client_account.route.ts
+++ src/routes/project/client_account.route.ts
@@ -17,9 +17,9 @@ class ClientAccountRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.get(this.path,[authMiddleware], this.clientAccount.getAllClientsAccounts);
-    this.router.get(`${this.path}/:clientId`, [authMiddleware], this.clientAccount.getClientAccount);
-    this.router.post(`${this.path}`, [authMiddleware, validationMiddleware(ClientAccountDto, 'body')], this.clientAccount.createClientAccount);
+    this.router.get(this.path,[permissionMiddleware('HR')], this.clientAccount.getAllClientsAccounts);
+    this.router.get(`${this.path}/:clientId`, [authMiddleware,  permissionMiddleware('HR')], this.clientAccount.getClientAccount);
+    this.router.post(`${this.path}`, [authMiddleware,  permissionMiddleware('HR'), validationMiddleware(ClientAccountDto, 'body')], this.clientAccount.createClientAccount);
   }
 }
 
