4b773a11913bb239bfbcdde3e4247f4efdf8fe1e
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-11-22 18:15:08 +0100

    update employee service

--- src/services/employee.service.ts
+++ src/services/employee.service.ts
@@ -357,13 +357,20 @@ class EmployeeService {
             }
           }
         }
-    ]);
+    ])
     return {genderCount}
   }
 
   public async getGenderDiversityRatio(): Promise<any>{
-    const genderRatio = this.getGenderCount()
-    return genderRatio
+      const gender = await Promise.all([this.getGenderCount()])
+      const numberOfMales = gender.map<any>(value => {
+        return value.genderCount.find(gender => gender._id==="male")
+      })[0].total
+      const numberOfFemales = gender.map<any>(value => {
+        return value.genderCount.find(gender => gender._id==="female")
+      })[0].total
+      const genderRatio = Math.ceil((numberOfMales/numberOfFemales))*100
+      return {genderRatio}
   }
 
   public async countEmployeesByDepartment(): Promise<any>{
