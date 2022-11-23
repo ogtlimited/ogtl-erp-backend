96fd477280f49fd5e4ecec4bb59785cf56c765dc
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-11-23 23:29:45 +0100

    Update client account service

--- src/services/project/client_account.service.ts
+++ src/services/project/client_account.service.ts
@@ -2,7 +2,7 @@
 import { IClientAccount } from '@/interfaces/project-interface/client_account.interface';
 import { HttpException } from '@exceptions/HttpException';
 import { isEmpty } from '@utils/util';
-import { ClientAccountDto } from '@/dtos/project/client_account.dto';
+import { ClientAccountDto, ResetClientAccountPasswordDto } from '@/dtos/project/client_account.dto';
 import clientAccountModel from '@/models/project/client_account.model';
 import clientModel from '@/models/project/client.model';
 const bcrypt = require('bcrypt')
@@ -17,6 +17,8 @@ import _ from "lodash"
 class ClientAccountService {
     private clientAccount: any = clientAccountModel;
     private client: any = clientModel
+    private jwtexpiresInHours: number = 24
+    private jwtExpiresIn: number = this.jwtexpiresInHours*60*60;
 
     public async findAllClientsAccounts(): Promise<IClientAccount[]> {
         const clientsAccount: IClientAccount[] = await this.clientAccount.find();
@@ -38,23 +40,25 @@ class ClientAccountService {
         const newclientAccount: any = await this.clientAccount.create(payload);
         const recordsWithoutPassword = _.omit(newclientAccount.toObject(),['password'])
         if(newclientAccount) {
-            const verifyToken = await this.createJwtToken(newclientAccount._id)
-            await this.sendMail(verifyToken.id, newclientAccount.email, newclientAccount.user_name)
+            const token = await this.createJwtToken(newclientAccount._id)
+            await this.sendMail(token, newclientAccount.email, newclientAccount.user_name)
             return recordsWithoutPassword ;
         }
     }
-    public async resetClientAccountPassword(clientAccountId: string, payload: ClientAccountDto): Promise<IClientAccount> {
+    public async resetDefaultClientAccountPasssword(payload: ResetClientAccountPasswordDto): Promise<ResetClientAccountPasswordDto> {
+        const token = payload.token;
+        const {clientAccount_id} = await this.verifyJwtToken(token)
         const salt = await bcrypt.genSalt(10)
-        const clientAccountExist = await this.clientAccount.findOne({_id: clientAccountId})
+        const clientAccountExist = await this.clientAccount.findOne({_id: clientAccount_id})
         if(!clientAccountExist) throw new HttpException(404, "Client account not found");
         if (isEmpty(payload)) throw new HttpException(400, "Bad request");
         const hashedPassword = await bcrypt.hash(payload.password, salt)
         const findclientAccountAndResetPassword = await this.clientAccount.updateOne(
             {
-            _id: clientAccountId
+            _id: clientAccount_id
             },
             {
-                $set: {password: hashedPassword, activated: payload.activated, spammy: payload.spammy}
+                $set: {password: hashedPassword, activated: true, spammy: false}
             });
             return findclientAccountAndResetPassword;
     }
@@ -71,18 +75,23 @@ class ClientAccountService {
         return findclientAccountRecords;
     }
    
-    private async sendMail(id: string, clientEmail: string, username: string): Promise<any> {
-        const clientAccountActivationNoticeObj = clientAccountActivationNotice(username)
+    private async sendMail(token: string, clientEmail: string, username: string, linkExpiresIn:number = this.jwtExpiresIn): Promise<any> {
+        const clientAccountActivationNoticeObj = clientAccountActivationNotice(username, linkExpiresIn)
         const { host }: dbConfig = config.get('dbConfig');
         const port = 3001
-        const url = `http://${host}:${port}/auth/activate?id=${id}`
+        const url = `http://${host}:${port}/auth/activate?token=${token}`
         const html = `<div><h1 style="color:#00c2fa">Outsource Global Technology Limited</h1><br></div>${clientAccountActivationNoticeObj.message}<a href=${url}>Click here to change your password</a>`
         return EmailService.sendMail(clientEmail,"hr@outsourceglobal.com",clientAccountActivationNoticeObj.subject,clientAccountActivationNoticeObj.message,html)
     }
     
-    private async createJwtToken(id: string, expiresIn: number = 24*60*60): Promise<any> {
+    private async createJwtToken(id: string, expiresIn: number = this.jwtExpiresIn): Promise<any> {
         const secretKey: string = await config.get('secretKey');
-        const token = await jwt.sign({ id }, secretKey,{ expiresIn: expiresIn });
+        const token = await jwt.sign({ clientAccount_id: id }, secretKey,{ expiresIn: expiresIn });
+        return token
+    }
+
+    private async verifyJwtToken(token: string): Promise<any> {
+        const secretKey: string = await config.get('secretKey');
         return await jwt.verify(token, secretKey);
     }
 }
