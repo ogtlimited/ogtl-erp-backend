df2baf4d84d99a21aadfe03ecaf58d3663831bfa
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-11-29 20:04:42 +0100

    Update employees route

--- src/routes/employee/employee.route.ts
+++ src/routes/employee/employee.route.ts
@@ -21,6 +21,7 @@ class EmployeesRoute implements Routes {
     this.router.get(`${this.path}`, authMiddleware, this.employeesController.getEmployees);
     this.router.get(`${this.path}/head-count`, authMiddleware, this.employeesController.getEmployeesHeadCount);
     this.router.get(`${this.path}/department/count`, authMiddleware, this.employeesController.countEmployeesByDepartment);
+    this.router.get(`${this.path}/department/:department_id`, authMiddleware, this.employeesController.getEmployeesByDepartment);
     this.router.get(`${this.path}/gender-count`, authMiddleware, this.employeesController.getGenderCount);
     this.router.get(`${this.path}/gender-ratio`, authMiddleware, this.employeesController.getGenderDiversityRatio);
     this.router.get(`${this.path}/employees/stats`, this.employeesController.getEmployeesByMonthStats);
