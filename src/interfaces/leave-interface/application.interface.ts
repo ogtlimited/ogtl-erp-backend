6b0d87fd8a8d2f223a5a99b469b96f0a5af5d243
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-12-06 12:24:34 +0100

    Update leave application interface

--- src/interfaces/leave-interface/application.interface.ts
+++ src/interfaces/leave-interface/application.interface.ts
@@ -1,15 +1,19 @@
 /* eslint-disable prettier/prettier */
 export interface ILeaveApplication{
     _id?: string;
-    employee_id?: string;
+    employee_id: string;
+    project_id?: string;
+    department_id: string;
     leave_type_id: string;
     from_date: Date;
     to_date : Date;
-    leave_approver?: string;
-    employee_project_id?: string;
-    // posting_date: Date;
-    reason: string;
-    status?: string;
+    leave_approver: string;
+    approval_level: number;
+    reason_for_application?: string;
+    status: string;
+    rejection_reason: string;
+    hr_stage: boolean;
+    acted_on: boolean;
 }
 export interface ILeaveCount{
    ogid: string;
