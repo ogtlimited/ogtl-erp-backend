3f0d939ff3a76e7f174b77ae9f4e32fc6c00953e
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-12-06 22:49:55 +0100

    Create leave_approval_level dto

---
+++ src/dtos/Leave/leave_approval_level.dto.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class LeaveApprovalLevelDto{
+    @IsString()
+    @IsNotEmpty()
+    public designation_id: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    public approval_level: number;
+}
