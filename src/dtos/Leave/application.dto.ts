c528e146bac98978838ffd9d1072f8725c600ad1
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-12-13 18:46:22 +0100

    Update leave application dto

--- src/dtos/Leave/application.dto.ts
+++ src/dtos/Leave/application.dto.ts
@@ -6,45 +6,18 @@ export class CreateLeaveApplicationDTO{
 
     @IsString()
     public leave_type: string;
-    
-    @IsString()
-    @IsNotEmpty()
-    public employee_id: string;
 
     @IsString()
     @IsOptional()
     public project_id: string;
 
     @IsString()
-    @IsNotEmpty()
-    public department_id: string;
-
-    @IsOptional()
-    public leave_approver: string | null;
-
-    @IsString()
     @IsOptional()
     public reason_for_application: string;
-
-    @IsBoolean()
-    @IsOptional()
-    public status: string;
 
     @IsString()
     @IsOptional()
     public rejection_reason: string;
-
-    @IsBoolean()
-    @IsOptional()
-    public hr_stage: boolean;
-
-    @IsBoolean()
-    @IsOptional()
-    public approval_level: number;
-
-    @IsBoolean()
-    @IsOptional()
-    public acted_on: boolean;
 
     @IsDateString()
     public from_date: Date;
@@ -59,10 +32,6 @@ export class UpdateLeaveApplicationDTO{
     public _id: string;
 
     @IsString()
-    @IsNotEmpty()
-    public employee_id: string;
-
-    @IsString()
     public leave_type: string;
 
     @IsDateString()
@@ -70,19 +39,9 @@ export class UpdateLeaveApplicationDTO{
 
     @IsDateString()
     public to_date : Date;
-    
-    @IsOptional()
-    public leave_approver: string | null;
 
     @IsDateString()
     public posting_date: Date;
-
-    @IsString()
-    public status: string;
-
-    @IsString()
-    @IsOptional()
-    public approval_level: string;
 
     @IsString()
     @IsOptional()
@@ -91,10 +50,6 @@ export class UpdateLeaveApplicationDTO{
     @IsString()
     @IsOptional()
     public rejection_reason: string;
-
-    @IsString()
-    @IsOptional()
-    public department_id: string;
 
     @IsString()
     @IsOptional()
