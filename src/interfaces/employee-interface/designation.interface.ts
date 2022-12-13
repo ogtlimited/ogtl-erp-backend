9f691bcd24d9e4c9326d743bb6605b98c16c749d
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-12-13 18:45:30 +0100

    Update designation interface

--- src/interfaces/employee-interface/designation.interface.ts
+++ src/interfaces/employee-interface/designation.interface.ts
@@ -3,5 +3,6 @@ export interface Designation{
     _id: string;
     designation: string;
     department_id: string;
-    project_id?: string
+    project_id?: string;
+    approval_level: string
 }
