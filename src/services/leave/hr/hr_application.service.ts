6b54562640ea7c4f028070e52ca6f25cee4f625d
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-12-14 09:20:55 +0100

    Update hr leave application service

--- src/services/leave/hr/hr_application.service.ts
+++ src/services/leave/hr/hr_application.service.ts
@@ -187,7 +187,8 @@ class HrLeaveApplicationService {
     const formattedFirstName = leaveApplicant.first_name.charAt(0) + leaveApplicant.first_name.toLowerCase().slice(1)
     const {status_message, status_subject} = leaveApplicationStatusMessage(formattedFirstName, status)
     const body = `<div><h1 style="color:#00c2fa">Outsource Global Technology Limited</h1><br></div>${status_message}`
-    EmailService.sendMail(leaveApplicant.company_email, "hr@outsourceglobal.com", status_subject, status_message, body)
+    // EmailService.sendMail(leaveApplicant.company_email, "hr@outsourceglobal.com", status_subject, status_message, body)
+    EmailService.sendMail("abubakarmoses@yahoo.com", "hr@outsourceglobal.com", status_subject, status_message, body)
   }
   private async updateTotalLeaveCount(leaveApplication){
     const leaveApplicationBusinessdays = this.getBusinessDatesCount(leaveApplication.from_date, leaveApplication.to_date)
@@ -217,16 +218,20 @@ class HrLeaveApplicationService {
     const employeesOnLeave: number = await this.application.find(matchBy).countDocuments()
     return employeesOnLeave
   }
-  public async getLeaveStatusCountForHrDashboard() {
-    const leaveCount = await this.application.find({
-      hr_stage: true,
-     '$group': {
-       'status': '$status', 
-       'total': {
-         '$count': {}
-       }
-     }
-  })
+  public async getLeaveStatusCountForHrDashboardAnalytics() {
+    const leaveCount = await this.application.aggregate([
+    {
+        '$match': {hr_stage: true}
+    },
+    {
+        '$group': {
+          '_id': '$status', 
+          'total': {
+            '$count': {}
+          }
+        }
+    }
+  ])
   return leaveCount
   }
 }
