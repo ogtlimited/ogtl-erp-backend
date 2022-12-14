099956a95ab8edb825927152617677a176d36abf
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-12-14 01:41:24 +0100

    Create hr leave application controller

---
+++ src/controllers/Leave/hr/hr_applications.controller.ts
@@ -0,0 +1,53 @@
+/* eslint-disable prettier/prettier */
+
+import { ILeaveApplication } from '@/interfaces/leave-interface/application.interface';
+import { NextFunction, Request, Response } from 'express';
+import HrLeaveApplicationService from '@/services/leave/hr/hr_application.service';
+
+class HrLeaveApplicationController {
+  public hrLeaveApplicationService = new HrLeaveApplicationService();
+  public getLeaveApplicationsForHr = async (req: any, res: Response, next: NextFunction) => {
+    try {
+      const leaveApplicationsForHr: ILeaveApplication[] = await this.hrLeaveApplicationService.getLeaveApplicationsForHr(req.query);
+      res.status(200).json({ data: leaveApplicationsForHr});
+    } catch (error) {
+      next(error);
+    }
+  };
+  public approveLeaveApplicationsByHr = async (req: any, res: Response, next: NextFunction) => {
+    try {
+      const {leaveId} = req.params
+      const leaveApplications: any = await this.hrLeaveApplicationService.approveLeaveApplicationsByHr(leaveId);
+      res.status(200).json({ data: leaveApplications});
+    } catch (error) {
+      next(error);
+    }
+  };
+  public rejectLeaveApplicationsByHr = async (req: any, res: Response, next: NextFunction) => {
+    try {
+      const {leaveId} = req.params
+      const leaveApplications: any = await this.hrLeaveApplicationService.rejectLeaveApplicationsByHr(leaveId);
+      res.status(200).json({ data: leaveApplications});
+    } catch (error) {
+      next(error);
+    }
+  };
+  public getLeaveStatusCountForHrDashboard = async (req: any, res: Response, next: NextFunction) => {
+    try {
+      const leaveStatus: any = await this.hrLeaveApplicationService.getLeaveStatusCountForHrDashboard();
+      res.status(200).json({ data: leaveStatus});
+    } catch (error) {
+      next(error);
+    }
+  };
+  public countEmployeesOnLeave = async (req: any, res: Response, next: NextFunction) => {
+    try {
+      const employeesOnLeave: any = await this.hrLeaveApplicationService.countEmployeesOnLeave();
+      res.status(200).json({ data: employeesOnLeave});
+    } catch (error) {
+      next(error);
+    }
+  };
+
+}
+export default HrLeaveApplicationController;
