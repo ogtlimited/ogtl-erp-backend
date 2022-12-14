d8bb4ab2d78c0ba3ed92c30e6d3364bf25df44d6
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-12-14 01:40:34 +0100

    Create leads leave application rejection route

--- src/routes/leave/leads/leads_leave_rejection.route.ts
+++ src/routes/leave/leads/leads_leave_rejection.route.ts
@@ -4,20 +4,19 @@ import  authMiddleware  from '@middlewares/auth.middleware';
 import { Router } from 'express';
 import  permissionMiddleware  from '@/middlewares/permission.middleware';
 import { Routes } from '@interfaces/routes.interface';
-import validationMiddleware from '@middlewares/validation.middleware';
-import LeaveApplicationController from '@/controllers/Leave/application.controller';
+import LeadsLeaveApplicationController from '@/controllers/Leave/leads/leads_application.controller';
 
 
 class LeadsLeaveRejectionRoute implements Routes {
   public path = '/leads-leave-rejection';
   public router = Router();
-  public leaveApplicationController = new LeaveApplicationController();
+  public leadsLeaveApplicationController = new LeadsLeaveApplicationController();
 
   constructor() {
     this.initializeRoutes();
   }
   private initializeRoutes() {
-    this.router.post(`${this.path}/:leaveId`, [authMiddleware], this.leaveApplicationController.rejectLeaveApplicationByLead);
+    this.router.post(`${this.path}/:leaveId`, [authMiddleware], this.leadsLeaveApplicationController.rejectLeaveApplicationByLead);
   }
 }
 export default LeadsLeaveRejectionRoute;
