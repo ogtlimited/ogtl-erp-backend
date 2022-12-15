4fe00342d5dbb4906d8be9ee4b19a423835007c3
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-12-15 12:52:23 +0100

    Update leave mailing service

--- src/services/leave/leave_mailing.service.ts
+++ src/services/leave/leave_mailing.service.ts
@@ -9,14 +9,16 @@ class LeaveMailingService{
         const formattedSupervisorFirstName = supervisor?.first_name.charAt(0) + supervisor?.first_name.toLowerCase().slice(1)
         const {message, subject} = leadsLeaveNotificationMessage(formattedSupervisorFirstName, formattedLeaveApplicantFirstName) 
         const body = `<div><h1 style="color:#00c2fa">Outsource Global Technology Limited</h1><br></div>${message}`
-        EmailService.sendMail(supervisor.company_email, "hr@outsourceglobal.com", subject, message, body)
+        // EmailService.sendMail(supervisor.company_email, "hr@outsourceglobal.com", subject, message, body)
+        EmailService.sendMail("abubakarmoses@yahoo.com", "hr@outsourceglobal.com", subject, message, body)
     }
     public async sendLeaveStatusNotificationMail(applicant, status, employeeModel){
         const leaveApplicant = await employeeModel.findOne({_id: applicant.employee_id})
         const formattedFirstName = leaveApplicant.first_name.charAt(0) + leaveApplicant.first_name.toLowerCase().slice(1)
         const {status_message, status_subject} = leaveApplicationStatusMessage(formattedFirstName, status)
         const body = `<div><h1 style="color:#00c2fa">Outsource Global Technology Limited</h1><br></div>${status_message}`
-        EmailService.sendMail(leaveApplicant.company_email, "hr@outsourceglobal.com", status_subject, status_message, body)
+        EmailService.sendMail("abubakarmoses@yahoo.com", "hr@outsourceglobal.com", status_subject, status_message, body)
+        // EmailService.sendMail(leaveApplicant.company_email, "hr@outsourceglobal.com", status_subject, status_message, body)
     }
 
 }
