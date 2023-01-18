a0ff673cb88be82fba8795ba13a8a4e401552d96
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-01-18 18:43:42 +0100

    Update leave application route

--- src/routes/leave/application.route.ts
+++ src/routes/leave/application.route.ts
@@ -23,7 +23,6 @@ class LeaveApplicationRoute implements Routes {
     this.router.get(`${this.path}`, authMiddleware, this.leaveApplicationController.getLeaveApplication);
     this.router.get(`${this.path}/leave-application-progress`, authMiddleware, this.leaveApplicationController.getLeaveApplicationProgress);
     this.router.get(`${this.path}/number-of-days`, authMiddleware, this.leaveApplicationController.getNumberOfLeaveDaysApplied);
-    this.router.get(`${this.path}/all-leave-approvers`, authMiddleware, this.leaveApplicationController.getAllLeaveAprovers);
     this.router.get(`${this.path}/team-member/all`, authMiddleware, this.leaveApplicationController.findAllTeamMembersLeave);
     this.router.get(`${this.path}/client-approval/:id`, authMiddleware, this.leaveApplicationController.findAllLeaveapplicationsClient);
     this.router.patch(`${this.path}/appeal-rejected-leave`, authMiddleware, this.leaveApplicationController.appealRejectedLeave);
