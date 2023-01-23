14225515aa2a2e378f6d82d26a2bdadc95917ad3
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-01-23 09:56:32 +0100

    Update validation middleware

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { plainToInstance } from 'class-transformer';
+import { plainToClass } from 'class-transformer';
 import { validate, ValidationError } from 'class-validator';
 import { RequestHandler } from 'express';
 import { HttpException } from '@exceptions/HttpException';
@@ -12,7 +12,7 @@ const validationMiddleware = (
   forbidNonWhitelisted = true,
 ): RequestHandler => {
   return (req, res, next) => {
-    validate(plainToInstance(type, req[value]), { skipMissingProperties, whitelist, forbidNonWhitelisted }).then((errors: ValidationError[]) => {
+    validate(plainToClass(type, req[value]), { skipMissingProperties, whitelist, forbidNonWhitelisted }).then((errors: ValidationError[]) => {
       if (errors.length > 0) {
         const message = errors.map((error: ValidationError) => Object.values(error.constraints)).join(', ');
         next(new HttpException(400, message));
