2952c4afec68dc45f628a16837838d9ae17f3d8d
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-01-24 03:08:57 +0100

    Update message.ts file

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -21,6 +21,17 @@ const acceptedOfferMessage ={
   subject: "New Employee"
 }
 
+const introductoryMail = (employee_firstname, employee_email) => {
+  return{
+  message: "Hello " + employee_firstname + ","
+    + "<br><br>" + " Welcome aboard, we look forward to working with you!"
+    + "<br><br>" + " Your company's email address is " + employee_email + "."
+    + " Please click on the link below to log into OGTL ERP,"
+    + "<br><br>" + " Thank you",
+  subject: "Employee Onboarding"
+  }
+}
+
 const ticketingMessage ={
   message: "A ticket has been created.\n  Please attend at the soonest ",
   subject: "New Ticket"
@@ -165,4 +176,4 @@ export { terminationMessage , promotionMessage, OfferMessage,acceptedOfferMessag
   offerMessageFunc, ticketingMessage,resolvedTicketingMessage,clientAccountActivationNotice, 
   birthdayMessage, leaveApplicationStatusMessage, leadsLeaveNotificationMessage, LeaveRejectionHrNotificationMessage,
   requestForLeaveModification, appealRejectedLeaveMessageToLead, appealRejectedLeaveMessageToTopLead,
-  leaveApplicationRejectionMessage };
+  leaveApplicationRejectionMessage, introductoryMail };
