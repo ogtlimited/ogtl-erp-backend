bd7775d328112101efb62f66f389d0f9396d8a51
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-02-09 09:24:20 +0100

    Update employee verification service

--- src/services/employee_verification.service.ts
+++ src/services/employee_verification.service.ts
@@ -2,12 +2,43 @@
 
 import { Employee } from '@interfaces/employee-interface/employee.interface';
 import EmployeeModel from '@models/employee/employee.model';
+import PersonalDetailModel from '@models/employee/personal-details.model';
+import DesignationModel from '@models/employee/designation.model';
+import projectModel from '@models/project/project.model';
+
 
 class EmployeeVerificationService {
-  public Employees = EmployeeModel;
+  private Employees = EmployeeModel;
+  private PersonalDetailModel = PersonalDetailModel;
+  private DesignationModel = DesignationModel;
+  private projectModel = projectModel;
   public async findEmployeeByOgId(ogid): Promise<any> {
-    const employeeExists = await this.Employees.exists({ogid});
-    return employeeExists;
+    const employee: Employee = await this.Employees.findOne({ ogid });
+    const role = employee.designation ? await this.DesignationModel.findById({ _id:employee?.designation }) : null
+    const campaign = employee.projectId? await this.projectModel.findById({ _id: employee?.projectId }) : null
+    const personalDetails = await this.PersonalDetailModel.findOne({ employee_id: employee?._id  })
+    return {
+      PictureUrl: employee.image,
+      StaffUniqueId: employee.ogid,
+      Email: employee.company_email,
+      FullName: this.formatFullname(employee, employee.first_name, employee.middle_name, employee.last_name),
+      // PhoneNumber: "Phone Number",
+      Gender: employee.gender,
+      MaritalStatus: personalDetails.marital_status,
+      DateOfBirth: personalDetails.date_of_birth,
+      // StateOfOrigin: "State of Origin",
+      StartDate: new Date(employee.date_of_joining),
+      Campaign: campaign? campaign.project_name : null,
+      Role: role ? role.designation : null
+    };
+  }
+
+  private formatFullname = (modelName,first_name, middle_name, last_name)=> {
+    first_name = modelName.first_name.charAt(0).toUpperCase() + modelName.first_name.toLowerCase().slice(1)
+    last_name = modelName.last_name.charAt(0).toUpperCase() + modelName.last_name.toLowerCase().slice(1)
+    middle_name = modelName.middle_name.charAt(0).toUpperCase() + modelName.middle_name.toLowerCase().slice(1)
+    return `${first_name} ${last_name} ${middle_name}` 
+    
   }
 }
 
