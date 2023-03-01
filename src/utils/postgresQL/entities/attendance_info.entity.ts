b2911df53f670bcd27e7fbc0cb070fe4390609cb
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-03-01 17:10:49 +0100

    adds entities

---
+++ src/utils/postgresQL/entities/attendance_info.entity.ts
@@ -0,0 +1,85 @@
+/* eslint-disable prettier/prettier */
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { Staff } from './staff.entity';
+
+
+@Entity()
+export class AttendanceInfo extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  // @IsNotEmpty()
+  ogid: string;
+
+  @Column()
+  // @IsNotEmpty()
+  image: string;
+
+  @Column()
+  // @IsNotEmpty()
+  default_shift: string;
+
+  @Column()
+  // @IsNotEmpty()
+  company_email: string;
+
+  @Column()
+  // @IsNotEmpty()
+  department: string;
+
+  @Column()
+  // @IsNotEmpty()
+  designation: string;
+
+  @Column()
+  // @IsNotEmpty()
+  reports_to: string;
+
+  @Column()
+  // @IsNotEmpty()
+  first_name: string;
+
+  @Column()
+  // @IsNotEmpty()
+  last_name: string;
+
+  @Column()
+  // @IsNotEmpty()
+  middle_name: string;
+
+  @Column()
+  // @IsNotEmpty()
+  gender: string;
+
+  @Column()
+  // @IsNotEmpty()
+  projectId: string;
+
+  @Column()
+  // @IsNotEmpty()
+  PhoneNumber: string;
+
+  @Column()
+  // @IsNotEmpty()
+  DateOfBirth: string;
+
+  @Column()
+  // @IsNotEmpty()
+  MaritalStatus: string;
+
+  @Column()
+  // @IsNotEmpty()
+  StateOfOrigin: string;
+
+  @Column()
+  // @IsNotEmpty()
+  Role: string;
+
+
+  @ManyToOne(() => Staff, (officer) => officer.commandAccessIds)
+  @JoinColumn()
+  officerId: OfficerEntity;
+
+
+}
