e866994959ee603cb63e6c52d5df029ec1357b94
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-03-02 09:44:22 +0100

    Creates typeorm config

---
+++ src/utils/postgreQL/index.ts
@@ -0,0 +1,22 @@
+import config from 'config';
+import { join } from 'path';
+import { DataSource } from 'typeorm';
+import { dbConfig } from './db.interface';
+import { Staff } from './staff.entity';
+import { AttendanceInfo } from './attendance_info.entity';  
+
+const dataSource = new DataSource({
+    type: 'postgres',
+    port: 5432,
+    host: 'localhost',
+    username: 'postgres',
+    password: "root",
+    database: 'ogtl_attendance',
+    logging: ['error'],
+    entities: [join(__dirname, '/../**/*.entity{.ts,.js}')],
+    // migrations: [join(__dirname, '../**/*.migration{.ts,.js}')],
+    // subscribers: [join(__dirname, '../**/*.subscriber{.ts,.js}')],
+
+});
+
+export default dataSource
