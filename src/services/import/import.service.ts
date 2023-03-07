2c7e199faa806745c0d988e6b8e19e178b28c2f9
anthugny <62931219+anthugny@users.noreply.github.com>
2023-03-07 13:26:50 +0100

    Update import.service.ts

--- src/services/import/import.service.ts
+++ src/services/import/import.service.ts
@@ -21,36 +21,16 @@ class ImportService {
       await fs.createReadStream('src/imports/csvs/leaves_request_forms.csv')
       .pipe(csv())
       .on('data', async function (data) {
-        // console.log(data['Email Address']);
-        // console.log(data);
-
-        
         
         try {
             const employee = await EmployeeModel.findOne({company_email: data['Email Address']}, {company_email:1})
-            count+=1
-            // console.log(employee);
-            
+              
             if (!employee){
 
               if(!emp_emails.hasOwnProperty(data['Email Address'])) {
                 fs.appendFileSync('src/imports/csvs/email_issues.csv', `${data['Email Address']}\n`);
                 emp_emails[data['Email Address']] = "exists"
-              }
-                // console.log(data);
-                
-            //   data.status = 'active'
-            //   delete data.reports_to
-            //   delete data.department
-            //   delete data.projectId
-            //   delete data.isAdmin
-            //   delete data.default_shift
-            //   delete data.designation
-            //   console.log(data.company_email);
-            //   await this.EmployeeModel.create(data)
-            // email_issues.push(employee)
-            // await fs.createReadStream('src/imports/csvs/email_issues.csv')
-            
+              }            
           }
           else{
             if(!emp_emails.hasOwnProperty(data['Email Address'])) {
