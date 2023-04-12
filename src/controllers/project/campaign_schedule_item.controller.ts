323f604893cc7bb979876f1b7beb47ef4dd7523d
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-04-12 18:25:31 +0100

    Update campaign_schedule_item.controller.ts

--- src/controllers/project/campaign_schedule_item.controller.ts
+++ src/controllers/project/campaign_schedule_item.controller.ts
@@ -2,13 +2,14 @@
 
 import { NextFunction, Request, Response } from 'express';
 import CampaignScheduleItemService from '@/services/project/campaign_schedule_item.service';
+import { ICampaignScheduleItem } from '@/interfaces/project-interface/campaign_schedule_item.interface';
 
 class CampaignScheduleItemController {
     private campaignScheduleItemService = new CampaignScheduleItemService()
 
     public findAllCampaignScheduleItems = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const campaignScheduleItems = this.campaignScheduleItemService.findAllCampaignScheduleItems();
+            const campaignScheduleItems: ICampaignScheduleItem[] = await this.campaignScheduleItemService.findAllCampaignScheduleItems();
             res.status(200).json({ data: campaignScheduleItems });
         } catch (error) {
             next(error);
@@ -17,7 +18,7 @@ class CampaignScheduleItemController {
     public updateCampaignScheduleItem = async (req: any, res: Response, next: NextFunction) => {
         try {
             const { campaignScheduleItemId } = req.param
-            const campaignScheduleItem = this.campaignScheduleItemService.updateCampaignScheduleItem(req.body, campaignScheduleItemId);
+            const campaignScheduleItem: ICampaignScheduleItem = await this.campaignScheduleItemService.updateCampaignScheduleItem(req.body, campaignScheduleItemId);
             res.status(200).json({ data: campaignScheduleItem });
         } catch (error) {
             next(error);
@@ -26,7 +27,7 @@ class CampaignScheduleItemController {
     public deleteCampaignScheduleItem = async (req: any, res: Response, next: NextFunction) => {
         try {
             const { campaignScheduleId } = req.param
-            const campaignScheduleItem = this.campaignScheduleItemService.deleteCampaignScheduleItem(campaignScheduleId);
+            const campaignScheduleItem: ICampaignScheduleItem = await this.campaignScheduleItemService.deleteCampaignScheduleItem(campaignScheduleId);
             res.status(200).json({ data: campaignScheduleItem });
         } catch (error) {
             next(error);
