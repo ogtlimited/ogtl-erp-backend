c87e1f5d4bc849a55b2e2ec339fd8ea61e65f2e4
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-04-12 14:28:28 +0100

    Update campaign_schedule.interface.ts

--- src/interfaces/project-interface/campaign_schedule.interface.ts
+++ src/interfaces/project-interface/campaign_schedule.interface.ts
@@ -1,5 +1,7 @@
+import { ICampaignScheduleItem } from "./campaign_schedule_item.interface";
 export interface ICampaignSchedule {
     _id?: string;
+    campaign_schedule_items: ICampaignScheduleItem[]
     title: string;
     owner: string;
     campaign: string;
