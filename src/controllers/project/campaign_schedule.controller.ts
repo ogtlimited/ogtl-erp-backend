cbf5430b2172065855767f974e89e140e9a9d7ef
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-04-12 14:28:23 +0100

    Update campaign_schedule.controller.ts

--- src/controllers/project/campaign_schedule.controller.ts
+++ src/controllers/project/campaign_schedule.controller.ts
@@ -2,13 +2,14 @@
 
 import { NextFunction, Request, Response } from 'express';
 import CampaignScheduleService from '@/services/project/campaign_schedule.service';
+import { ICampaignSchedule } from '@/interfaces/project-interface/campaign_schedule.interface';
 
 class CampaignScheduleController {
     private campaignScheduleService = new CampaignScheduleService()
 
     public findAllCampaignSchedule = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const campaignSchedules = this.campaignScheduleService.findAllCampaignSchedule();
+            const campaignSchedules: ICampaignSchedule[]  = await this.campaignScheduleService.findAllCampaignSchedule();
             res.status(200).json({ data: campaignSchedules });
         } catch (error) {
             next(error);
@@ -17,16 +18,15 @@ class CampaignScheduleController {
     public findCampaignScheduleByOwnersId = async (req: any, res: Response, next: NextFunction) => {
         try {
             const { _id } = req.user
-            const campaignSchedule = this.campaignScheduleService.findCampaignScheduleByOwnersId(_id);
-            res.status(200).json({ data: campaignSchedule });
+            const campaignSchedules: ICampaignSchedule[] = await this.campaignScheduleService.findCampaignScheduleByOwnersId(_id);
+            res.status(200).json({ data: campaignSchedules });
         } catch (error) {
             next(error);
         }
     };
     public createCampaignSchedule = async (req: any, res: Response, next: NextFunction) => {
         try {
-            const { _id } = req.user
-            const campaignSchedule = this.campaignScheduleService.createCampaignSchedule(req.body, _id);
+            const campaignSchedule: ICampaignSchedule = await this.campaignScheduleService.createCampaignSchedule(req.body, req.user);
             res.status(200).json({ data: campaignSchedule });
         } catch (error) {
             next(error);
@@ -35,7 +35,7 @@ class CampaignScheduleController {
     public updateCampaignSchedule = async (req: any, res: Response, next: NextFunction) => {
         try {
             const { campaignScheduleId } = req.param
-            const campaignSchedule = this.campaignScheduleService.updateCampaignSchedule(req.body, campaignScheduleId);
+            const campaignSchedule: ICampaignSchedule = await this.campaignScheduleService.updateCampaignSchedule(req.body, campaignScheduleId);
             res.status(200).json({ data: campaignSchedule });
         } catch (error) {
             next(error);
@@ -44,7 +44,7 @@ class CampaignScheduleController {
     public deleteCampaignSchedule = async (req: any, res: Response, next: NextFunction) => {
         try {
             const { campaignScheduleId } = req.param
-            const campaignSchedule = this.campaignScheduleService.deleteCampaignSchedule(campaignScheduleId);
+            const campaignSchedule: ICampaignSchedule = await this.campaignScheduleService.deleteCampaignSchedule(campaignScheduleId);
             res.status(200).json({ data: campaignSchedule });
         } catch (error) {
             next(error);
