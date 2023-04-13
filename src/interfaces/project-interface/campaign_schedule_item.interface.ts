c3748c19ef600a5514975e29282a38ee70b57b03
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-04-13 12:01:06 +0100

    Update campaign_schedule_item.interface.ts

--- src/interfaces/project-interface/campaign_schedule_item.interface.ts
+++ src/interfaces/project-interface/campaign_schedule_item.interface.ts
@@ -1,6 +1,10 @@
 export interface ICampaignScheduleItem {
     _id?: string;
     day: string;
+    start: string;
+    end: string;
+    huddles: boolean;
+    huddleTime: string;
     off: boolean;
     campaign_schedule: string
 }
