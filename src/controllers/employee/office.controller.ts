803b5bc4642bc9ed02933125029df76d99bca983
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-04-18 13:39:47 +0100

    Update office.controller.ts

--- src/controllers/employee/office.controller.ts
+++ src/controllers/employee/office.controller.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 import { IDepartment } from '@/interfaces/employee-interface/department.interface';
 import OfficeService from '@/services/employee/office.service';
 import { IProject } from '@/interfaces/project-interface/project.interface';
+import { Employee } from '@/interfaces/employee-interface/employee.interface';
 
 class OfficeController {
     public OfficeService = new OfficeService();
@@ -11,6 +12,14 @@ class OfficeController {
         try {
             const office: IDepartment | IProject = await this.OfficeService.createOffice(req.body);
             res.status(201).json({ data: office, message: 'Succesfully created' });
+        } catch (error) {
+            next(error);
+        }
+    };
+    public findEmployeesByOffice = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const employees: Employee[] = await this.OfficeService.findEmployeesByOffice(req.query);
+            res.status(201).json({ data: employees });
         } catch (error) {
             next(error);
         }
