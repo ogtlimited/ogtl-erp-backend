f132fa6d658cc2b3db7504d6080ccb0f91444bd1
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-04-26 18:26:31 +0100

    Update attendance.route.ts

--- src/routes/attendance/attendance.route.ts
+++ src/routes/attendance/attendance.route.ts
@@ -25,7 +25,7 @@ class AttendanceRoute implements Routes {
         this.router.get(`${this.path}`,[authMiddleware], this.AttendanceController.getDepartmentAttendance);
         this.router.get(`${this.path}/from-postgres`, [authMiddleware], this.AttendanceController.findAllExternalDatabaseAttendance);
         this.router.get(`${this.path}/from-postgres/for-hr`, [authMiddleware], this.AttendanceController.findExternalDatabaseAttendanceByOgId);
-        this.router.get(`${this.path}/captured-employees`, [authMiddleware], this.AttendanceController.findCapturedEmployeesOnBiometricDatabaseAndThereShiftStatus);
+        this.router.get(`${this.path}/captured-biometrics`, [authMiddleware], this.AttendanceController.findCapturedEmployeesOnBiometricDatabaseAndThereShiftStatus);
         this.router.get(`${this.path}/employee/:ogId`,[authMiddleware], this.AttendanceController.getEmployeeAttendance);
         this.router.get(`${this.path}/:id`,[authMiddleware], this.AttendanceController.getAttendanceById);
         this.router.post(`${this.path}`,[authMiddleware, validationMiddleware(CreateAttendanceDto, 'body')], this.AttendanceController.createAttendance);
