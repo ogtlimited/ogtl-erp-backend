b8ea7ae5fb0d8766c71ae6774ac06206dbef10c8
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-04-26 18:05:45 +0100

    creates a new attendance service method that gets captured biometric staff and their shift status

--- src/routes/attendance/attendance.route.ts
+++ src/routes/attendance/attendance.route.ts
@@ -25,10 +25,10 @@ class AttendanceRoute implements Routes {
         this.router.get(`${this.path}`,[authMiddleware], this.AttendanceController.getDepartmentAttendance);
         this.router.get(`${this.path}/from-postgres`, [authMiddleware], this.AttendanceController.findAllExternalDatabaseAttendance);
         this.router.get(`${this.path}/from-postgres/for-hr`, [authMiddleware], this.AttendanceController.findExternalDatabaseAttendanceByOgId);
+        this.router.get(`${this.path}/captured-employees`, [authMiddleware], this.AttendanceController.findCapturedEmployeesOnBiometricDatabaseAndThereShiftStatus);
         this.router.get(`${this.path}/employee/:ogId`,[authMiddleware], this.AttendanceController.getEmployeeAttendance);
         this.router.get(`${this.path}/:id`,[authMiddleware], this.AttendanceController.getAttendanceById);
         this.router.post(`${this.path}`,[authMiddleware, validationMiddleware(CreateAttendanceDto, 'body')], this.AttendanceController.createAttendance);
-        // this.router.post(`${this.path}/bulk`,[authMiddleware], this.AttendanceController.CreateBulkAttendance);
         this.router.post(`${this.path}/postgresdb-bulk-upload`,[authMiddleware], this.AttendanceController.uploadMultipleAttendanceRecord);
         this.router.patch(`${this.path}`, [authMiddleware, validationMiddleware(UpdateAttendanceDto, 'body')], this.AttendanceController.updateAttendance);
     }
