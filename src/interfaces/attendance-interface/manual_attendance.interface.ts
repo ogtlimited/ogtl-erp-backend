cf570866d9948385c9b7937b0347f7c282e40bd3
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-05-10 14:44:11 +0100

    Create manual_attendance.interface.ts

---
+++ src/interfaces/attendance-interface/manual_attendance.interface.ts
@@ -0,0 +1,9 @@
+export interface IManualAttendance {
+    ogId?: string;
+    attendance_id: string;
+    clockInTime?: string;
+    clockOutTime?: String;
+    departmentId?: string;
+    campaignId?: string;
+    reason?: string
+}
