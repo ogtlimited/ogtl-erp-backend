36417f2f8f023f5d490a4628ef0a5bae2ed1c706
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-05-10 14:48:34 +0100

    Update manual_attendance.interface.ts

--- src/interfaces/attendance-interface/manual_attendance.interface.ts
+++ src/interfaces/attendance-interface/manual_attendance.interface.ts
@@ -1,6 +1,6 @@
 export interface IManualAttendance {
     ogId?: string;
-    attendance_id: string;
+    attendance_id_from_external_db: string;
     clockInTime?: string;
     clockOutTime?: String;
     departmentId?: string;
