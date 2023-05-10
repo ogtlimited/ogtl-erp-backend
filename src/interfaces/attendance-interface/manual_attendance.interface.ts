7fedfa9b72f961a9d9d28d3f04757818f650505b
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-05-10 16:42:09 +0100

    Delete manual_attendance.interface.ts

--- src/interfaces/attendance-interface/manual_attendance.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IManualAttendance {
-    ogId?: string;
-    attendance_id_from_external_db: string;
-    clockInTime?: string;
-    clockOutTime?: String;
-    departmentId?: string;
-    campaignId?: string;
-    reason?: string
-}
